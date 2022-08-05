2b7d84f0296d01fd343045c3833d6f47827970c4
TellKobayashi <haruki.kobayashi.tf@ambl.co.jp>
2022-08-05 06:41:18 +0000

    動的コンテンツを追加して、RSpecを導入しました

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,10 @@
-require 'test_helper'
+require 'test_helper'   #ライブラリや、別のソースファイルを読み込むrequire関数で、
+                        #test/test_helperを呼び出す
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest   
+        #Action~がStatic~に継承されている。一番後ろの::IntegrationTestは、IntegrationTest絶対的な
+        #位置指定になる。つまり、Integtration~はAction~の親クラスだよーってこと。
+  test "should get home" do #テストの名前
     get static_pages_home_url
     assert_response :success
   end
